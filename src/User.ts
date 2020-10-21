93263890a0d4a4a28d09a531cbfcb08d23556f64
clark-lindsay <clarklindsay96@gmail.com>
2020-10-21 14:54:45 -0400

    Add a wall, containing the posts of followed users
    I added an optional parameter to the publishPost method to allow for
    posting with a different date because I failed to figure out how to mock
    the global date object within the tests. Without some way of changing
    the date, all of the posts would have the same time down to the
    millisecond, and sorting them by date would have no effect.

--- src/User.ts
+++ src/User.ts
@@ -1,22 +1,39 @@
 export class User {
   name: string;
   private timeline: Post[];
+  private following: User[];
 
   constructor({ name }: { name: string }) {
     this.name = name;
     this.timeline = [];
+    this.following = [];
   }
 
-  publishPost(content: string): void {
+  follow(userToFollow: User): void {
+    this.following.push(userToFollow);
+  }
+
+  publishPost(content: string, datePublished?: Date): void {
     this.timeline.push({
       content,
-      datePublished: new Date(),
+      datePublished: datePublished || new Date(),
       author: this,
     });
   }
 
   getTimeline(): Post[] {
     return [...this.timeline].reverse();
+  }
+
+  getWall(): Post[] {
+    let result: Post[] = this.getTimeline();
+    this.following.forEach(
+      (user) => (result = [...result, ...user.getTimeline()])
+    );
+
+    return result.sort((a: Post, b: Post) => {
+      return b.datePublished.valueOf() - a.datePublished.valueOf();
+    });
   }
 }
 
--- tests/User.test.ts
+++ tests/User.test.ts
@@ -25,3 +25,43 @@ describe("the Timeline feature", () => {
     expect(timeOfSecondPost).toBeGreaterThanOrEqual(timeOfFirstPost);
   });
 });
+
+describe("the Following feature", () => {
+  describe("the Users wall", () => {
+    it("should return an aggregated list of the posts of all the users that they follow, sorted in revers-publishing order", () => {
+      const alice = new User({ name: "Alice" });
+      alice.publishPost(
+        "I love the weather today.",
+        new Date(new Date().setMinutes(0))
+      );
+
+      const bob = new User({ name: "Bob" });
+      bob.publishPost("Darn! We lost!", new Date(new Date().setMinutes(3)));
+      bob.publishPost("Good game though.", new Date(new Date().setMinutes(4)));
+
+      const charlie = new User({ name: "Charlie" });
+      charlie.publishPost(
+        "I'm in New York today! Anyone want to have a coffee?",
+        new Date(new Date().setMinutes(5))
+      );
+      charlie.follow(alice);
+      charlie.follow(bob);
+
+      const charlieWall = charlie.getWall();
+
+      expect(charlieWall[0].content).toEqual(
+        "I'm in New York today! Anyone want to have a coffee?"
+      );
+      expect(charlieWall[0].author.name).toEqual("Charlie");
+
+      expect(charlieWall[1].content).toEqual("Good game though.");
+      expect(charlieWall[1].author.name).toEqual("Bob");
+
+      expect(charlieWall[2].content).toEqual("Darn! We lost!");
+      expect(charlieWall[2].author.name).toEqual("Bob");
+
+      expect(charlieWall[3].content).toEqual("I love the weather today.");
+      expect(charlieWall[3].author.name).toEqual("Alice");
+    });
+  });
+});
